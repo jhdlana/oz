111b9a715ff54454df448e6e20d9b555f36ca82d
jhdlana <ljh06031@gmail.com>
2024-05-26 20:12:41 +0900

    DetailPage-9일차과제

--- React/react-apple-app/src/pages/DetailPage/index.jsx
+++ React/react-apple-app/src/pages/DetailPage/index.jsx
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from '../../api/axios'
 import { imageBasePath } from "../../constant"
+import './DetailPage.css'
 
 const DetailPage = () => {
     console.log(useParams())
 
-    const {moiveId} = useParams()
+    const {movieId} = useParams()
     const [movie, setMovie] = useState(null)
 
     useEffect(()=> {
         async function fetchData() {
-            const response = await axios.get(`/movie/${moiveId}`)
-            setMovie(reponse.data)
+            const response = await axios.get(`/movie/${movieId}`)
+            setMovie(response.data)
         }
         fetchData()
     }, [movieId])
@@ -21,12 +22,42 @@ const DetailPage = () => {
 
 
   return (
-    <section>
-        <img
-            src={`${imageBasePath}${movie.backdrop_path}`}
-            alt="detail"
-        />
-    </section>
+    <div className="wrapper-detail">
+        <section className="detail-section">
+            <img
+                className="detail-img"
+                src={`${imageBasePath}${movie.backdrop_path}`}
+                alt="detail"
+            />
+            <div className='detail__content'>
+                {/* <p className='modal__details'>
+                
+                </p> */}
+                <h2 className='detail__title'>
+                    {movie.title}
+                    {console.log(movie)}
+                </h2>
+                <p className='detail__title-name'>평점: {movie.vote_average}</p>
+                <h3 className="detail__genre">장르</h3>
+                    <p className="detail__genre-tag">
+                        {movie.genre_ids}
+                        {movie.genres.map((genre)=> {
+                            {console.log(genre)}
+                            {console.log(genre.name)}
+                            return (
+                                <span className="genre-name" key={genre.id}> {genre.name} </span>
+                            )
+                        })}
+                    </p>
+                
+                <h3 className='detail__title-name'>
+                    콘텐츠 설명
+                </h3>
+                <p className="detail__overview">{movie.overview}</p>
+                
+            </div>
+        </section>
+    </div>
   )
 }
 
